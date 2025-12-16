ce506f206340152fdcef3e0a4a90029826ccbef1
alex010b <151800458+alex010b@users.noreply.github.com>
2025-12-16 02:38:49 -0500

    Modify database connection string
    Updated the database connection string for security.

--- database.go
+++ database.go
@@ -10,7 +10,7 @@ import (
 var db *gorm.DB
 
 func dbConnect() {
-	dsn := "root:ImABall!@#@@(@11212@tcp(127.0.0.1:3306)/lockedand?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:"
 	db, _ = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	fmt.Println("connected to db")
